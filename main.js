f34a80fe6d34c8fc21b5dc27d7f8d725b8e2b353
Kunju Perath <kperath@gmail.com>
2020-05-02 04:12:28 -0400

    pretty much finished basic layout. now to trigger color customization

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ Vue.component("app-form-field", {
     props: ['color', 'label', 'field_type'],
     template: `
     <div class="flex justify-center">
-    <div :class="'flex justify-center mt-8 border-8 border-' +  color + '-900 rounded-full'">
+    <div :class="'flex justify-center m-4 border-8 border-' +  color + '-900 rounded-full'">
         <label for="label" :class="'flex items-center bg-' + color + '-300 w-40 capitalize px-4 py-2 rounded-l-full text-' + color + '-900'">{{ label }}</label>
         <div class="bg-white rounded-r-full">
             <input id="label" :placeholder="'your ' + label" class="capitalize m-4" :type="field_type">
@@ -15,7 +15,7 @@ Vue.component("app-form-field", {
 var app = new Vue({
     el: "#app",
     data: {
-        formColor: "indigo"
+        formColor: "green"
     }
 })
 
