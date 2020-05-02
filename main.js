0c6a4db5fa18fc57d34b336ebcfbcfe02e976c9d
Kunju Perath <kperath@gmail.com>
2020-05-01 23:27:06 -0400

    all fields are working. On to about page and profile card

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 Vue.component("app-form-field", {
-    props: ['color', 'label'],
+    props: ['color', 'label', 'field_type'],
     template: `
     <div class="flex justify-center">
     <div :class="'flex justify-center mt-8 border-8 border-' +  color + '-900 rounded-full'">
-        <label for="first-name" :class="'flex items-center bg-' + color + '-300 w-40 capitalize px-4 py-2 rounded-l-full text-' + color + '-900'">first name</label>
+        <label for="first-name" :class="'flex items-center bg-' + color + '-300 w-40 capitalize px-4 py-2 rounded-l-full text-' + color + '-900'">{{ label }}</label>
         <div class="bg-white rounded-r-full">
-            <input placeholder="your first name" class="border-red-900 capitalize m-4">
+            <input :placeholder="'your ' + label" class="capitalize m-4" :type="field_type">
         </div>
         </div>
     </div>
@@ -15,8 +15,7 @@ Vue.component("app-form-field", {
 var app = new Vue({
     el: "#app",
     data: {
-        formColor: "teal",
-        formLabel: "first name"
+        formColor: "blue"
     }
 })
 
