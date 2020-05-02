9f796472abe0ceb7f7934fe570b9db17ee1a2147
Kunju Perath <kperath@gmail.com>
2020-05-01 22:57:32 -0400

    fixed color changing in template woooo

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 Vue.component("app-form-field", {
-    props: ['color'],
+    props: ['color', 'label'],
     template: `
     <div class="flex justify-center">
-    <div class="flex justify-center mt-8 border-8 border-blue-900 rounded-full">
-        <label for="first-name" class="flex items-center bg-blue-300 w-40 capitalize px-4 py-2 rounded-l-full text-blue-900">first name</label>
+    <div :class="'flex justify-center mt-8 border-8 border-' +  color + '-900 rounded-full'">
+        <label for="first-name" :class="'flex items-center bg-' + color + '-300 w-40 capitalize px-4 py-2 rounded-l-full text-' + color + '-900'">first name</label>
         <div class="bg-white rounded-r-full">
             <input placeholder="your first name" class="border-red-900 capitalize m-4">
         </div>
-    </div>
+        </div>
     </div>
     `
 })
@@ -15,7 +15,8 @@ Vue.component("app-form-field", {
 var app = new Vue({
     el: "#app",
     data: {
-        formColor: "yellow"
+        formColor: "teal",
+        formLabel: "first name"
     }
 })
 
